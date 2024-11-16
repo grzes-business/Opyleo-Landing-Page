7f69eb524fb1787ad231fbc9d90fc16b3c6f2bf9
grzes-business <grzes.business@gmail.com>
2024-11-16 11:24:39 +0100

    Created a basic responsive swiperJS with placeholder slides

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['swiper', 'swiper/*']
+    }
+  }
 });
