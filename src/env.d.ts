e2e11307387201d5cdb7da02f89bd7b6b0fc6b73
grzes-business <grzes.business@gmail.com>
2024-12-11 19:14:17 +0100

    env.d.ts setup with api_max_page variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-    readonly API_URL: string; 
+    readonly API_URL: string;
+    readonly API_MAX_PAGE: number;
 }
 
 interface ImportMeta {
